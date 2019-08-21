54b275aadc2e6de14b4b7494cbe1eda9e1c8a9a9
=Alejandro Baez <baezdalex@gmail.com>
2019-08-21 13:38:16 -0500

    Componente Header de la App

---
+++ src/components/Organisms/Header.jsx
@@ -0,0 +1,34 @@
+import React ,{createRef} from 'react'
+import {NavLink} from 'react-router-dom'
+import PrivateMenu from '../Molecules/PrivateMenu';
+import PublicMenu from '../Molecules/PublicMenu';
+
+//Manejo de referencias en React
+const menu = createRef()
+const toggleMenu = ()=> menu.current.classList.toggle('show')
+
+const Header = () => (
+  <header className="main-header">
+    <div className="ed-grid s-grid-5 lg-grid-4">
+      <div className="s-cols-4 lg-cols-1 s-cross-center" >
+       <NavLink to="/">
+         <img className="main-logo" src="https://ed.team/static/images/logo.svg" alt="Logo"/>
+       </NavLink> 
+      </div>
+      <div className="s-cols-1 lg-cols-3 s-cross-center s-main-end">
+        <nav className="main-menu" ref={menu}>
+         {
+           localStorage.getItem('token') 
+           ? <PrivateMenu/>
+           : <PublicMenu/>
+         }
+        </nav>
+        <div className="main-menu-toggle to-l"
+        onClick={()=> toggleMenu()}
+        ></div>
+      </div>
+    </div>
+  </header>
+)
+
+export default Header
